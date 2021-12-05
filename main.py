8b207624dc2c2357272d42b753f33ccab2fcd018
Balagoore <grishasheiko@gmail.com>
2021-12-05 23:58:00 +0300

    https://github.com/Balagoore/homew.git

--- main.py
+++ main.py
@@ -1,132 +1,34 @@
-import requests
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-result = requests.get("https://pokemon/ditto").json()
-
-pokemon["id"], pokemon["name"], pokemon["height"], pokemon["weight"]
-
-class BasePokemon:
-
-    def __init__(self, name: str):
-        self.name = name
+    def dist(self, other):
+        if isinstance(other, Point):
+            return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
 
 
-class Pokemon(BasePokemon):
+class Vector(Point):
+    def length(self):
+        return self.dist(Point(0, 0))
 
-    def __init__(self, name: str, id: float, height: float, weight: float):
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
 
-        BasePokemon.__init__(self, name)
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
 
-        self.__name = name
-        self.__id = id
-        self.__height = height
-        self.__weight = weight
-    @property
-    def Id(self):
-        return self.__id
-    @Id.setter
-    def Id(self, id):
-        self.__id = id
-    def Height(self):
-        return self.__height
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
 
-    @Height.setter
-    def Height(self, height):
-        self.__height = height
+    def __rmul__(self, other):
+        return self * other
 
-    def Weight(self):
-        return self.__weight
-
-    @Weight.setter
-    def Weight(self, weight):
-        self.__weight = weight
-  #  Id = property(__getID, __setID)
-  #  Height = property(__getHeight, __setHeight)
-  #  Weight = property(__getWeight, __setWeight)
     def __str__(self):
-        return f'Pokemon name is {self.name}, {self.id}, {self.height}, {self.weight}'
+        return f'Vector<x={self.x}, y={self.y}>'
 
-    PokemonStats()
-
-
-class PokemonStats:
-
-   def __init__(self, hp: float, attack: str, defense: str, sp_attack: str, sp_defense: str, speed: float):
-        self.__hp = hp
-        self.__attack = attack
-        self.__defense = defense
-        self.__sp_attack = sp_attack
-        self.__sp_defense = sp_defense
-        self.__speed = speed
-
-   @property
-   def HP(self):
-       return self.__hp
-
-   @HP.setter
-   def HP(self, hp):
-       self.__hp = hp
-
-   def Attack(self):
-       return self.__attack
-
-   @Attack.setter
-   def Attack(self, attack):
-       self.__attack = attack
-
-   def Defense(self):
-       return self.__defense
-
-   @Defense.setter
-   def Defense(self, defense):
-       self.__defense = defense
-
-   def Spatt(self):
-       return self.__sp_attack
-   @Spatt.setter
-   def Spatt(self, sp_attack):
-       self.__sp_attack = sp_attack
-
-   def Spdef(self):
-       return self.__sp_defense
-
-   @Spdef.setter
-   def Spdef(self, sp_defense):
-       self.__sp_defense = sp_defense
-
-   def Speed(self):
-       return self.__speed
-   @Speed.setter
-
-   def Speed(self, speed):
-       self.__speed = speed
-
-   def __str__(self):
-       return f'Pokemon description is {self.hp}, {self.attack}, {self.defense}, {self.sp_attack}, {self.sp_defense}'
-
-class PokeAPI(Pokemon):
-    def __init__(self, get_full, Pt):
-        Pokemon.__init__(self, name, id, height, weight)
-        self.get_full = get_full
-        self.Pt = Pt
-        Pt = False
-
-    def get_pokemon(self, name):
-        return pokemon
-
-    def get_all(self, get_full):
-
-        return basepokemon if get_full == Pt else print()
-
-    def get_pokemon(self, get_full) -> Iterator:
-
-        while get_full != Pt:
-            yield pok .PokemonStats
-
-
-
-pok = Pokemon(name, id, height, weight)
-pok.str()
-basepok = BasePokemon(name)
-Ditto = Pokemon()
-
-print(Ditto.get_full)
